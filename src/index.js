080a7ef88ab58f63a35a42666c4ddd0454eae63a
Miro <mr.mladenoff@gmail.com>
2018-11-30 11:28:55 -0800

    Lifecycle Methods - ComponentDidMount

--- src/index.js
+++ src/index.js
@@ -2,20 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
+  state = {
+    lat: null,
+    errorMessage: ""
+  };
 
-    this.state = { lat: null, errorMessage: "" };
-
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-      position => {
-        this.setState({ lat: position.coords.latitude });
-      },
-      err => {
-        this.setState({ errorMessage: err.message });
-      }
+      position => this.setState({ lat: position.coords.latitude }),
+      err => this.setState({ errorMessage: err.message })
     );
   }
+
   render() {
     if (this.state.errorMessage && !this.state.lat) {
       return <div>Error: {this.state.errorMessage}</div>;
