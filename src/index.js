9c16b8f20f04e9d82f52a14a5ad8d8849b39f1cc
Miro <mr.mladenoff@gmail.com>
2018-11-30 13:34:26 -0800

    Avoiding Conditional in Render

--- src/index.js
+++ src/index.js
@@ -16,16 +16,19 @@ class App extends React.Component {
     );
   }
 
-  render() {
+  // Helper Method
+  renderContent() {
     if (this.state.errorMessage && !this.state.lat) {
       return <div>Error: {this.state.errorMessage}</div>;
     }
-
     if (!this.state.errorMessage && this.state.lat) {
       return <SeasonDisplay lat={this.state.lat} />;
     }
-
     return <Spinner message="Please accept location request" />;
+  }
+
+  render() {
+    return <div className="border red">{this.renderContent()}</div>;
   }
 }
 
